008588d8a5ccf8e17d1550cfc7fe16a605877a7c
Mario <mario.martins@sonda.com>
2020-06-25 11:06:28 -0300

    Criando estrutura

---
+++ src/br/com/inovation/comparator/Aluno.java
@@ -0,0 +1,69 @@
+package br.com.inovation.comparator;
+
+import java.time.LocalDate;
+
+public class Aluno {
+
+    private Integer codigo;
+    private String nome;
+    private Integer idade;
+    private Character sexo;
+    private Integer anoNascimento;
+
+    public Aluno(Integer codigo, String nome, Integer idade, Character sexo, Integer anoNascimento) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.anoNascimento = anoNascimento;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Character getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+
+    public Integer getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(Integer anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", anoNascimento=" + anoNascimento +
+                '}';
+    }
+}
---
+++ src/br/com/inovation/comparator/MainComparador.java
@@ -0,0 +1,39 @@
+package br.com.inovation.comparator;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainComparador {
+
+    public static void main(String[] args) {
+        List<Aluno> alunos = new ArrayList<>();
+        preenCherAlunos(alunos);
+
+        System.out.println(alunos);
+    }
+
+    private static void preenCherAlunos(List<Aluno> alunos) {
+        Aluno aluno1 = new Aluno(1, "Mario", 43, 'M', 1977);
+        alunos.add(aluno1);
+        Aluno aluno2 = new Aluno(2, "Andreia", 43, 'F', 1976);
+        alunos.add(aluno2);
+        Aluno aluno3 = new Aluno(3, "Milena", 21, 'F', 1998);
+        alunos.add(aluno3);
+        Aluno aluno4 = new Aluno(4, "Rafaela", 16, 'F', 2003);
+        alunos.add(aluno4);
+        Aluno aluno5 = new Aluno(5, "Luciana", 46, 'F', 1974);
+        alunos.add(aluno5);
+        Aluno aluno6 = new Aluno(6, "Renata", 45, 'F', 1975);
+        alunos.add(aluno6);
+        Aluno aluno7 = new Aluno(7, "Marcio", 42, 'M', 1978);
+        alunos.add(aluno7);
+        Aluno aluno8 = new Aluno(8, "Juliana", 39, 'F', 1981);
+        alunos.add(aluno8);
+        Aluno aluno9 = new Aluno(9, "Mario", 68, 'M', 1952);
+        alunos.add(aluno9);
+        Aluno aluno10 = new Aluno(10, "Gloria", 64, 'F', 1956);
+        alunos.add(aluno10);
+
+    }
+}
